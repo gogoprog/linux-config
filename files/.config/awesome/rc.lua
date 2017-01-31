7befbe2eb0fa54e5dfd5f3be5370100f99e48443
Gauthier Billot <gauthier.billot@fishingcactus.com>
2017-01-31 22:34:57 +0100

    Updated

--- files/.config/awesome/rc.lua
+++ files/.config/awesome/rc.lua
@@ -679,3 +679,7 @@ client.connect_signal("focus",
     end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+os.execute("volumeicon &")
+os.execute("qbat &")
+os.execute("nm-applet &")
